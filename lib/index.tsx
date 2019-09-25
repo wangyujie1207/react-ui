eb3954593c769d821f6b17750ecbd8ef431421a0
wangyujie1207 <82414199@qq.com>
2019-09-25 17:43:55 +0800

    del test-jest

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,11 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import Icon from './icon/icon';
 
-const fn = (e: React.MouseEvent) => {
-  console.log('fn', e.target);
-};
-
-ReactDOM.render(<div>
-  <Icon name="alipay" onClick={fn}/>
-</div>, document.querySelector('#root'));
+export {Icon}
