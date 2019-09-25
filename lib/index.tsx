8da86b8df7a89d15278c4cacb6405b1641eeeb35
wangyujie1207 <82414199@qq.com>
2019-09-25 11:07:06 +0800

    创建icon

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,7 @@
-import Button from './button';
-const a = 1;
-console.log('hi')
-export {Button, a}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Icon from './icon';
+
+ReactDOM.render(<div>
+  <Icon name="wechat" />
+</div>, document.body);
