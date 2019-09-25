baa1baadf42687f44a49afbd6b0429f706342a99
wangyujie1207 <82414199@qq.com>
2019-09-25 16:22:05 +0800

    icon测试用例

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Icon from './icon';
+import Icon from './icon/icon';
 
 const fn = (e: React.MouseEvent) => {
   console.log('fn', e.target);
